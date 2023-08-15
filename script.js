fcc7fadb3629adb602d805cd715561b062a212d7
Kerttu Immonen <kerttu.immonen63@gmail.com>
2023-08-15 20:47:08 +0300

    js

--- script.js
+++ script.js
@@ -1,78 +1,85 @@
 const form = document.getElementById("form");
-
 const id = document.getElementById('id');
 const salasana = document.getElementById('salasana');
 const nimi = document.getElementById('nimi');
-const osoite = document.getElementById('osoite');
-/*const maa = document.getElementById("maat").value;	oikea muoto laita */
+/**const osoite = document.getElementById('osoite'); */
+/*const maa = document.getElementById("maat").;	oikea muoto laita 
 const postinumero = document.getElementById('postinumero');
-const email = document.getElementById('email');
-/**const sukupuoli = document.getElementById('sukupuoli').value;	 */
-/**const kieli = document.getElementById('kieli').value;	 */
-const lisatietoja = document.getElementById('lisatietoja');
+const email = document.getElementById('email');*/
+/**const sukupuoli = document.getElementById('sukupuoli').;	 */
+/**const kieli = document.getElementById('kieli').;	 
+const lisatietoja = document.getElementById('lisatietoja');*/
 
-/**form.addEventListener('lähetä', e => { //error
-	e.preventDefault();
+function validateForm() {
+	var status = false;
+
+	const id = document.getElementById("id").value;
+	const salasana = document.getElementById("salasana").value;
+	const nimi = document.getElementById("nimi").value;
+	/**
+	const osoite = osoite..trim();
+	maa 
+	const postinumero = postinumero..trim();
+	const email = email..trim();*/
+	/**const sukupuoli = sukupuoli..trim();
+	const kieli = kieli..trim(); */
+	/**const lisatietoja = lisatietoja..trim(); */
+
+	if(id == '') {	//DONE
+		document.getElementById("idPaikka").innerHTML = ("tyhjä");
+		status = false;
+	} else if (id.length < 6){
+		document.getElementById("idPaikka").innerHTML = ("Liian lyhyt");
+		status = false;
+	} else {
+		document.getElementById("idPaikka").innerHTML = ("läpi");
+		status = true;
+	}
+
+	if(salasana == '') { 	//DONE
+		document.getElementById("salasanaPaikka").innerHTML = "ei saa olla tyhjä";
+		status = false;
+
+	} else {
+		document.getElementById("salasanaPaikka").innerHTML = ("läpi");
+		status = true;
+	}
+
+	if(nimi == '') {	//>DONE
+		document.getElementById("nimiPaikka").innerHTML = "Kirjoita nimi"
+		status = false;
+	} else {
+		document.getElementById("nimiPaikka").innerHTML = "läpi";
+		status = true;
+	}
+	return status;
 	
-	checkInputs();
-}); */
 
-function checkInputs() {
-
-	const idValue = id.value.trim();
-	const salasanaValue = salasana.value.trim();
-	const nimiValue = nimi.value.trim();
-	const osoite = osoite.value.trim();
-	/**maa */
-	const postinumero = postinumero.value.trim();
-	const emailValue = email.value.trim();
-	/**const sukupuoli = sukupuoli.value.trim();
-	const kieli = kieli.value.trim(); */
-	const lisatietoja = lisatietoja.value.trim();
-
-	if(idValue === '') {
-		setErrorFor(id, 'ID ei voi olla tyhjä');
-	} else if (id < 6){
-		setErrorFor(id, 'ID on liian lyhyt');
-	} else {
-		setSuccessFor(id);
-	}
-
-	if(salasanaValue === '') {
-		setErrorFor(salasana, 'salasana ei voi olla tyhjä');
-	} else {
-		setSuccessFor(salasana);
-	}
-
-	if(nimiValue === '') {
-		setErrorFor(nimi, 'Nimi ei voi olla tyhjä');
-	} else {
-		setSuccessFor(nimi);
-	}
 	/*osoite */
 
 	/*maa */
 
 	/*postinumero */
-
-	if(emailValue === '') {
+/** 
+	if(email === '') {
 		setErrorFor(email, 'Sähköposti ei voi olla tyhjä');
-	} else if (!isEmail(emailValue)) {      /*got no glue */ 
+	} else if (!isEmail(email)) {     
 		setErrorFor(email, 'Ei hyväksyttävä sähköposti');
 	} else {
 		setSuccessFor(email);
-	}
-	/**sukupuoli */
+	}*/
+	/**sukupuoli 
 	if(document.getElementById('mies').checked == true) {   
          document.write("");   
     } else {  
          document.write("");   
-    } 
+    } */
 	/**kieli */
 
 	/**lisätietoja-valinnainen */
 	
 }
+/**
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
 	const small = formControl.querySelector('small');
@@ -82,7 +89,7 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-control success';
-}	
+}	 
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
+}*/
